8ca1ef02cf9faad99ace030bd8189e3bb726c457
justintika <ntik0026@uni.sydney.edu.au>
2026-08-24 15:36:16 +1000

    Create BatteryMonitor.cpp

---
+++ Lab1/A5.RobotController/BatteryMonitor.cpp
@@ -0,0 +1,18 @@
+#include "BatteryMonitor.h"
+
+BatteryMonitor::BatteryMonitor()
+    : mLevel(100)
+{
+}
+
+void BatteryMonitor::Run()
+{
+    mLevel -= 5;          // battery drains each cycle
+    if(mLevel < 0)
+        mLevel = 0;
+}
+
+void BatteryMonitor::Report()
+{
+    std::cout << "Battery level: " << mLevel << "%" << std::endl;
+}
