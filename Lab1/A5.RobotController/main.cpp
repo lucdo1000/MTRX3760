1f509a9998a43ff3edc2af982a2af8180c44b694
justintika <ntik0026@uni.sydney.edu.au>
2026-08-24 15:59:28 +1000

    Remove LineSensor and add RegenerativeBreaking

--- Lab1/A5.RobotController/main.cpp
+++ Lab1/A5.RobotController/main.cpp
@@ -8,13 +8,13 @@ int main()
     Controller robot;
 
     DriveMotor motor;
-    LineSensor sensor;
     BatteryMonitor battery;
-
+    RegenerativeBreaking brakes(&motor, &battery)
+    
     robot.AddSubsystem(&motor);
-    robot.AddSubsystem(&sensor);
     robot.AddSubsystem(&battery);
-
+    robot.AddSubsystem(&brakes);
+    
     for(int i = 0; i < 3; ++i)
     {
         robot.RunCycle();
