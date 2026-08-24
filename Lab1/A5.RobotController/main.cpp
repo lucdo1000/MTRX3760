add19b36f4e2b79ca662760a725be0a50c753383
justintika <ntik0026@uni.sydney.edu.au>
2026-08-24 16:25:50 +1000

    Update main.cpp

--- Lab1/A5.RobotController/main.cpp
+++ Lab1/A5.RobotController/main.cpp
@@ -12,8 +12,8 @@ int main()
     RegenerativeBraking brakes(&motor, &battery);
     
     robot.AddSubsystem(&motor);
-    robot.AddSubsystem(&battery);
     robot.AddSubsystem(&brakes);
+    robot.AddSubsystem(&battery);
     
     for(int i = 0; i < 5; ++i)
     {
