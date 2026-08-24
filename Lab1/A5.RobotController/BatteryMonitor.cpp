7d12a81c71286772fc01afe126e0461ca63a71f6
justintika <ntik0026@uni.sydney.edu.au>
2026-08-24 16:05:53 +1000

    Add battery drain and regeneration methods

--- Lab1/A5.RobotController/BatteryMonitor.cpp
+++ Lab1/A5.RobotController/BatteryMonitor.cpp
@@ -22,3 +22,27 @@ void BatteryMonitor::Charge(int amount)
     mLevel += amount;
     if(mLevel > 100) mLevel = 100;
 }
+
+void BatteryMonitor::ProgressiveDrain(int motorSpeed)
+{
+    // Base drain
+    int drain = 5;
+
+    // Extra drain based on speed
+    drain += motorSpeed / 5;   // every +20 speed adds +1 drain
+
+    mLevel -= drain;
+    if(mLevel < 0) mLevel = 0;
+}
+
+void BatteryMonitor::RegenCharge(int speedDrop)
+{
+    // Base regen
+    int charge = 2;
+
+    // More regen for sharper braking
+    charge += speedDrop / 10;   // every -10 speed adds +1 charge
+
+    mLevel += charge;
+    if(mLevel > 100) mLevel = 100;
+}
