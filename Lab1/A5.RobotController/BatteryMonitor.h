6516350ab6036716893001133575b07bbc64c0fc
justintika <ntik0026@uni.sydney.edu.au>
2026-08-24 16:03:52 +1000

    Update BatteryMonitor.h

--- Lab1/A5.RobotController/BatteryMonitor.h
+++ Lab1/A5.RobotController/BatteryMonitor.h
@@ -15,6 +15,10 @@ public:
     // For regenerative breaking
     void Charge(int amount);
 
+    // Higher speed = Higher battery drain
+    void ProgressiveDrain(int motorSpeed);
+    void RegenCharge(int speedDrop);
+
 private:
     int mLevel;   // battery percentage
 };
